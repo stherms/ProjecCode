a734ac45f56eea51f14a5718ad25724f044b4071
JorgeGonSan <162482388+JorgeGonSan@users.noreply.github.com>
2024-03-08 18:11:55 +0100

    Tercera prueba

--- ProjectCode/src/A_MenuInicial.java
+++ ProjectCode/src/A_MenuInicial.java
@@ -2,5 +2,6 @@ public class A_MenuInicial {
     public static void main(String[] args){
         System.out.println("HOLA PEPITO");
         System.out.println("HOLA JOSE");
+        System.out.println("Hola Alberto");
     }
 }
