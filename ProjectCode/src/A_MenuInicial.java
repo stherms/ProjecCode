0d8831fd827bc479eab2895c6967cd31d21cf000
Albertlp93 <151084990+Albertlp93@users.noreply.github.com>
2024-03-11 11:45:06 +0000

    menuinicio

--- ProjectCode/src/A_MenuInicial.java
+++ ProjectCode/src/A_MenuInicial.java
@@ -1,7 +1,29 @@
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
 public class A_MenuInicial {
+    ArrayList<String> MenuInicio = new ArrayList<>();
     public static void main(String[] args){
-        System.out.println("HOLA PEPITO");
-        System.out.println("HOLA JOSE");
-        System.out.println("Hola Alberto");
+        A_MenuInicial menuInicial = new A_MenuInicial();
+        menuInicial.MenuInicial();
+    }
+
+    public void addMenuInicio(){
+        MenuInicio.add("EXCUSIONES");
+        MenuInicio.add("SOCIOS");
+        MenuInicio.add("INSCRIPCIONES");
+        MenuInicio.add("SALIR");
+    }
+    
+
+    private void MenuInicial(){
+        if(MenuInicio.size()<=0){
+            addMenuInicio();
+        }
+        StringBuilder mensajeInicio = new StringBuilder();
+        mensajeInicio.append("MENU DE INICIO:\n");
+        for(int i=0; i < MenuInicio.size(); i++){
+            mensajeInicio.append((i + 1) + ") " + MenuInicio.get(i) + "\n");
+        }
+        int eleccion = Integer.parseInt(JOptionPane.showInputDialog(mensajeInicio.toString()));
     }
 }
